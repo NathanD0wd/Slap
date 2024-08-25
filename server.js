a2ba0f1e150facfe53f9c0d538cba21cb8325a0d
NathanD0wd <dowdnj@gmail.com>
2024-08-25 15:44:15 +1000

    Something

--- server.js
+++ server.js
@@ -71,9 +71,14 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is listening on http://localhost:3000');
+// Set the server to listen on the port provided by Heroku or default to 3000
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+// server.listen(3000, () => {
+//     console.log('Server is listening on http://localhost:3000');
+// });
 
 
 // GAME LOGIC
